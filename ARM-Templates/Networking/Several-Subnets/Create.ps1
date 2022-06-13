e66f0dc6e63fe1057429d5d4e6c9f64b4fa8c9de
Yoav <yoavklein25@gmail.com>
2022-06-13 11:32:19 +0300

    Improved Several-Subnets

---
+++ ARM-Templates/Networking/Several-Subnets/Create.ps1
@@ -0,0 +1,13 @@
+
+$ResourceGroupName = "1-f90e6e66-playground-sandbox"
+$Arguments = @{
+    vnetName = "myVNet"
+    location = "East US"
+    vnetAddressPrefix = "10.0.0.0/16"
+    subnets = @(
+        @{ name = "mySubnet"; prefix = "10.0.1.0/24" }
+        @{ name = "anotherSubnet"; prefix = "10.0.2.0/24" }
+    )
+}
+
+New-AzResourceGroupDeployment -Name VirtualNetwork -ResourceGroupName $ResourceGroupName @Arguments -TemplateFile Unified.json
---
+++ ARM-Templates/Networking/Several-Subnets/README.md
@@ -0,0 +1,7 @@
+# VNet and Subnets - Flexible
+---
+
+In this template, we utilize the `copy` function to enable the user to create as many subnets
+as he likes.
+For each subnet created, a corresponding NetworkSecurityGroup is created also.
+
