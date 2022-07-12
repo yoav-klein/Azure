1b497923932d7de8211f68bc26016d7b840a2ab4
YOAV KLAIN <YOAVKL@rafael.co.il>
2022-07-12 08:38:41 +0300

    Networking::Load Balancer:: removing lbIPName parameter

--- ARM-Templates/Networking/Load-Balancer/Create.ps1
+++ ARM-Templates/Networking/Load-Balancer/Create.ps1
@@ -1,9 +1,8 @@
 
 $Arguments = @{
-    ResourceGroupName = "1-ab7d8dd8-playground-sandbox"
+    ResourceGroupName = "1-0c5ee206-playground-sandbox"
     location = "eastus"
     lbName = "myLB"
-    lbIPName = "lbIP"
 }
 
 New-AzResourceGroupDeployment -Name "LoadBalancer"  @Arguments -TemplateFile LoadBalancer-Template.json
