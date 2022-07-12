91691ae3646e14e161575d824c35f558095ae5fc
YOAV KLAIN <YOAVKL@rafael.co.il>
2022-07-12 13:53:15 +0300

    Networking::Load Balancer:: adding parameter for backend pool

--- ARM-Templates/Networking/Load-Balancer/Create.ps1
+++ ARM-Templates/Networking/Load-Balancer/Create.ps1
@@ -3,6 +3,7 @@ $Arguments = @{
     ResourceGroupName = "1-0c5ee206-playground-sandbox"
     location = "eastus"
     lbName = "myLB"
+    lbBackendName = "myBackendAddressPool"
 }
 
 New-AzResourceGroupDeployment -Name "LoadBalancer"  @Arguments -TemplateFile LoadBalancer-Template.json
