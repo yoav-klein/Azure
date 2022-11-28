d6f8a421a6ba46853411ab118424e42baab11738
Yoav Klein <yoavkl@rafael.co.il>
2022-11-28 16:41:23 +0200

    My implemenation - start

---
+++ App-Registration/my-impl/app.py
@@ -0,0 +1,19 @@
+
+@app.route("/")
+def index():
+
+@app.route("/login")
+def login():
+
+@app.route("/logout")
+def logout():
+
+@app.route("/authorized")
+def authorized():
+    
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
