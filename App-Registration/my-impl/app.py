2c99d7223d93c8f4e0e9b4ac2799bf7ae1c537eb
Yoav Klein <yoavkl@rafael.co.il>
2022-11-28 16:46:12 +0200

    my impl

--- App-Registration/my-impl/app.py
+++ App-Registration/my-impl/app.py
@@ -1,16 +1,23 @@
 
 @app.route("/")
 def index():
+    # if not logged in: redirect to login
+    # render_template("index.html", user=user)
 
 @app.route("/login")
 def login():
+    # send authorization request to authorization endpoint
+    # redirect to /authorized
 
 @app.route("/logout")
 def logout():
 
 @app.route("/authorized")
 def authorized():
-    
+    # take the request parmaters - authorization code and status
+    # contact token endpoint to request for a ID token and access token
+    # redirect to index
+
 
 def main():
     
